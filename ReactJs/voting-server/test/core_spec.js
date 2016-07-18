86a4643de722409add2d2015d8cf66275e0c6f2f
Breno Teixeira <brenotx@gmail.com>
2016-07-18 15:12:08 -0300

    Moving to The Next Pair

--- ReactJs/voting-server/test/core_spec.js
+++ ReactJs/voting-server/test/core_spec.js
@@ -30,8 +30,7 @@ describe('application logic', () => {
 
     describe('next', () => {
 
-        describe('takes the next to entries under vote', () => {
-
+        it('takes the next to entries under vote', () => {
             const state = Map({
                 entries: List.of(
                     'Trainspotting',
@@ -46,7 +45,46 @@ describe('application logic', () => {
                 }),
                 entries: List.of('Sunshine')
             }));
+        });
 
+        it('puts winner of current vote back to entries', () => {
+            const state = Map({
+                vote: Map({
+                    pair: List.of('Trainspotting', '28 Days Later'),
+                    tally: Map({
+                        'Trainspotting': 4,
+                        '28 Days Later': 2
+                    })
+                }),
+                entries: List.of('Sunshine', 'Millions', '127 Hours')
+            });
+            const nextState = next(state);
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair: List.of('Sunshine', 'Millions')
+                }),
+                entries: List.of('127 Hours', 'Trainspotting')
+            }));
+        });
+
+        it('puts both from tied vote back to entries', () => {
+            const state = Map({
+                vote: Map({
+                    pair: List.of('Trainspotting', '28 Days Later'),
+                    tally: Map({
+                        'Trainspotting': 3,
+                        '28 Days Later': 3
+                    })
+                }),
+                entries: List.of('Sunshine', 'Millions', '127 Hours')
+            });
+            const nextState = next(state);
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair: List.of('Sunshine', 'Millions')
+                }),
+                entries: List.of('127 Hours', 'Trainspotting', '28 Days Later')
+            }));
         });
 
     });
@@ -54,7 +92,6 @@ describe('application logic', () => {
     describe('vote', () => {
 
         it('creates a tally for the voted entry', () => {
-
             const state = Map({
                 vote: Map({
                     pair: List.of('Trainspotting', '28 Days Later')
@@ -71,7 +108,6 @@ describe('application logic', () => {
                 }),
                 entries: List()
             }));
-
         });
 
         it('adds to existing tally for the voted entry', () => {
